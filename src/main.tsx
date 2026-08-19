0fbc53f17b93185e46ed6bf571aa88758d0d4130
COCO Cat <53858352+printfChins@users.noreply.github.com>
2026-08-19 12:44:19 +0800

    Initialize screen wake lock in main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import {initScreenWakeLock} from './utils/screenWakeLock.ts'
 import './index.css';
 
+initScreenWakeLock();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
